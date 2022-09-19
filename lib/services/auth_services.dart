c9c8e8260823bd111ddfe398fe853055888f1dbb
DevBees <82749068+DevBees@users.noreply.github.com>
2022-09-20 01:18:17 +0530

    Update auth_services.dart

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -1,11 +1,8 @@
 // ignore_for_file: unused_local_variable, use_build_context_synchronously, avoid_print, prefer_const_constructors
 
-//TODO 6: import firebase auth and google sign in packages
+//TODO 6a: import firebase auth and google sign in packages
 
-import 'package:flutter/material.dart';
-
+import 'package:flutter/material.dart'; //To invoke a snackbar
 
 //TODO 7: Create Auth Services class and write function to sign IN/Sign up using google account
-class AuthServices {
-  
-}
+class AuthServices {}
